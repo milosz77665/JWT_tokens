d6690627d87893fc42c32d35e4c224cedc108292
Miłosz Marchewka <milosz77665@gmail.com>
2024-01-12 18:30:47 +0100

    add handleLogout function

--- frontend/src/app/(home)/page.js
+++ frontend/src/app/(home)/page.js
@@ -1,12 +1,22 @@
+"use client";
+
 import styles from "@/styles/home/homePage.module.css";
 import DocumentGrid from "@/components/home/DocumentGrid";
 import LogoutIcon from "@/assets/icons/LogoutIcon";
+import { deleteCookie } from "cookies-next";
+import { useRouter } from "next/navigation";
 
 export default function HomePage() {
+  const router = useRouter();
+  function handleLogout() {
+    deleteCookie("jwt");
+    router.push("/login");
+  }
+
   return (
     <div className={styles.homePageContainer}>
       <h1 className={styles.title}>Document Viewer</h1>
-      <LogoutIcon className={styles.logoutIcon} title="Logout" />
+      <LogoutIcon className={styles.logoutIcon} title="Logout" onClick={handleLogout} />
       <DocumentGrid />
     </div>
   );
