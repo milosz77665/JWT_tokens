0865a27ab61f7216c224af638dc714eaea8b07d6
Miłosz Marchewka <milosz77665@gmail.com>
2024-01-13 16:07:22 +0100

    change backend logic

--- backend/app.js
+++ backend/app.js
@@ -1,86 +1,75 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const fs = require('fs');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const data = require("./database.json");
 
 const app = express();
 const port = 5000;
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(bodyParser.json());
 
-// Odczyt database.js
-let data;
-try {
-  data = JSON.parse(fs.readFileSync('database.json', 'utf-8'));
-  console.debug("Users: ", data.users);
-  console.debug("Documents: ", data.documents);
-} catch (err) {
-  console.error('File reading error:', err.message);
-  process.exit(1);
-}
-// Tworzenie listy użytkowników i wszystkich dostępnych dokumentów
+const adminRole = "admin";
+const userRole = "user";
+
+// Tworzenie listy użytkowników i obiektu z dokumentami
 const users = data.users;
 const documents = data.documents;
 
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
-  const user = users.find(u => u.username === username && u.password === password);
+  const user = users.find((u) => u.username === username && u.password === password);
 
   if (user) {
-    // Generowanie tokenu - zawiera identyfikator użytkownika i dostęp do określonych plików
+    // Generowanie tokenu - zawiera id użytkownika, nazwę użytkownika oraz jego rolę
     // secretKey - tajny klucz do podpisywania tokenów, używany do weryfikacji
-    // expiresIn - czas ważności 
-    const token = jwt.sign({ userId: user.id, access: user.documents }, 'secretKey', { expiresIn: '1h' });
+    // expiresIn - czas ważności
+    const token = jwt.sign({ userId: user.id, username: user.username, role: user.role }, "secretKey", {
+      expiresIn: "1h",
+    });
     console.debug("Token: ", token);
 
-    // Przesłanie nazw wszystkich dostępnych dokumentów do użytkownika
-    const allDocumentNames = Object.keys(documents).map(docId => ({
-      id: docId,
-      name: documents[docId][0].title
-    }));
-    console.log("Documents: ", allDocumentNames);
-
-    res.status(200).json({ token, documents: allDocumentNames });
+    res.status(200).json({ token, userId: user.id, username: user.username });
   } else {
-    res.status(401).json({ message: 'Incorrect data' });
+    res.status(401).json({ message: "Incorrect data" });
   }
 });
 
-const verifyTokenAndCheckDocumentAccess = (req, res, next) => {
+const verifyToken = (req, res, next) => {
   // Odczytanie tokenu
-  const token = req.headers.authorization.split(' ')[1];
+  const token = req.headers.authorization.split(" ")[1];
 
-  jwt.verify(token, 'secretKey', (err, decoded) => {
+  // Weryfikajca
+  jwt.verify(token, "secretKey", (err, decoded) => {
     if (err) {
-      return res.status(401).json({ message: 'Verification error' });
+      return res.status(401).json({ message: "Verification error" });
     }
 
-    const documentId = req.params.documentId;
-
-    // Sprawdzamy czy ma dostęp do tego co chce 
-    if (decoded.access.includes(Number(documentId))) {
-      req.decodedToken = decoded;
-      next();
-    } else {
-      return res.status(403).json({ message: 'No access' });
-    }
+    req.decodedToken = decoded;
+    next();
   });
 };
 
-app.get('/documents/:documentId', verifyTokenAndCheckDocumentAccess, (req, res) => {
-  const documentId = req.params.documentId;
-  console.debug("DocumentId: ", documentId);
-  const documentContent = documents[documentId][0].content;
-  console.debug("Content: ", documentContent);
-  res.json({ documentContent, userId: req.decodedToken.userId });
-});
+app.get("/documents", verifyToken, (req, res) => {
+  const userId = req.decodedToken.userId;
+  const username = req.decodedToken.username;
+  const role = req.decodedToken.role;
 
-app.post('/logout', (req, res) => {
-  console.debug("Logout");
-  res.status(200).json({ message: 'Logout successful' });
+  console.debug(`User Role: ${role}`);
+
+  if (role === adminRole) {
+    const allDocuments = Object.values(documents).flat();
+    console.debug(`Documents for ${username}: ${allDocuments.map((document) => document.title)}`);
+
+    res.status(200).json({ documents: allDocuments });
+  } else if (role === userRole) {
+    console.debug(`Documents for ${username}: ${documents[userId].map((document) => document.title)}`);
+
+    res.status(200).json({ documents: documents[userId] });
+  } else {
+  }
 });
 
 app.listen(port, () => {
